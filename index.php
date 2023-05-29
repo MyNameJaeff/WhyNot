be2442e068e0ce6cbc4e4c3d38986463faad7962
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-05-29 10:32:03 +0200

    l

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logobox">
+            <img src="" alt="webbsiteLogo">
+        </div>
+    </header>
+    <main>
+        <div class="inputBox">
+            <form method="POST" class="meatInputs">
+                <div class="meatInput meat1">
+
+                </div>
+                <div class="meatInput meat2">
+
+                </div>
+                <div class="meatInput meat3">
+
+                </div>
+                <input type="submit" value="Beräkna">
+            </form>
+        </div>
+        <div class="waterBoxes">
+            <div class="waterBox water1">
+
+            </div>
+            <div class="waterBox water2">
+
+            </div>
+            <div class="waterBox water3">
+
+            </div>
+        </div>
+        <div class="yearBox">
+            <div class="lifebox life1">
+                <p class="lifevalue1"></p>
+            </div>
+            <div class="lifebox life2">
+                <p class="lifevalue2"></p>
+            </div>
+            <div class="lifebox life3">
+                <p class="lifevalue3"></p>
+            </div>
+        </div>
+    </main>
+    <?php
+
+    ?>
+</body>
+</html>
